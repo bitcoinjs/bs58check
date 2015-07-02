55534ace40b5ed3d663bb8a8ffd7671beb7fb611
Daniel Cousens <github@dcousens.com>
2015-07-02 23:12:20 +1000

    use standard style

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var base58 = require('bs58')
 var createHash = require('create-hash')
 
 // SHA256(SHA256(buffer))
-function sha256x2(buffer) {
+function sha256x2 (buffer) {
   buffer = createHash('sha256').update(buffer).digest()
   return createHash('sha256').update(buffer).digest()
 }
 
 // Encode a buffer as a base58-check encoded string
-function encode(payload) {
+function encode (payload) {
   var checksum = sha256x2(payload).slice(0, 4)
 
   return base58.encode(Buffer.concat([
@@ -20,16 +20,16 @@ function encode(payload) {
 }
 
 // Decode a base58-check encoded string to a buffer
-function decode(string) {
+function decode (string) {
   var buffer = new Buffer(base58.decode(string))
 
   var payload = buffer.slice(0, -4)
   var checksum = buffer.slice(-4)
   var newChecksum = sha256x2(payload).slice(0, 4)
 
-  if (!Array.prototype.every.call(newChecksum, function(x, i) {
-    return x === checksum[i]
-  })) {
+  for (var i = 0; i < newChecksum.length; ++i) {
+    if (newChecksum[i] === checksum[i]) continue;
+
     throw new Error('Invalid checksum')
   }
 
--- test/index.js
+++ test/index.js
@@ -1,30 +1,32 @@
+/* global describe it */
+
 var assert = require('assert')
 var base58check = require('../')
 
 var fixtures = require('./fixtures')
 
-describe('bs58check', function() {
-  describe('decode', function() {
-    fixtures.valid.forEach(function(f) {
-      it('can decode ' + f.string, function() {
+describe('bs58check', function () {
+  describe('decode', function () {
+    fixtures.valid.forEach(function (f) {
+      it('can decode ' + f.string, function () {
         var actual = base58check.decode(f.string).toString('hex')
 
         assert.equal(actual, f.payload)
       })
     })
 
-    fixtures.invalid.forEach(function(f) {
-      it('throws on ' + f, function() {
-        assert.throws(function() {
+    fixtures.invalid.forEach(function (f) {
+      it('throws on ' + f, function () {
+        assert.throws(function () {
           base58check.decode(f)
         }, /Invalid checksum/)
       })
     })
   })
 
-  describe('encode', function() {
-    fixtures.valid.forEach(function(f) {
-      it('can encode ' + f.string, function() {
+  describe('encode', function () {
+    fixtures.valid.forEach(function (f) {
+      it('can encode ' + f.string, function () {
         var actual = base58check.encode(new Buffer(f.payload, 'hex'))
 
         assert.strictEqual(actual, f.string)
