a92d9d4fe00b9ccb9812145954c1880c3ff3f3c1
Daniel Cousens <github@dcousens.com>
2016-01-25 01:25:08 +1100

    index: use XOR, avoid extra slice

--- index.js
+++ index.js
@@ -25,14 +25,12 @@ function decode (string) {
 
   var payload = buffer.slice(0, -4)
   var checksum = buffer.slice(-4)
-  var newChecksum = sha256x2(payload).slice(0, 4)
+  var newChecksum = sha256x2(payload)
 
-  if (checksum[0] !== newChecksum[0] ||
-      checksum[1] !== newChecksum[1] ||
-      checksum[2] !== newChecksum[2] ||
-      checksum[3] !== newChecksum[3]) {
-    throw new Error('Invalid checksum')
-  }
+  if (checksum[0] ^ newChecksum[0] |
+      checksum[1] ^ newChecksum[1] |
+      checksum[2] ^ newChecksum[2] |
+      checksum[3] ^ newChecksum[3]) throw new Error('Invalid checksum')
 
   return payload
 }
