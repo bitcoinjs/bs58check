12dbe58f0946967c3f1b5e9d002e91c5b55ecc66
Kirill Fomichev <fanatid@ya.ru>
2016-03-31 08:23:09 +0300

    crypto-hashing instead create-hash

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 'use strict'
-
 var base58 = require('bs58')
-var createHash = require('create-hash')
-
-// SHA256(SHA256(buffer))
-function sha256x2 (buffer) {
-  var tmp = createHash('sha256').update(buffer).digest()
-  return createHash('sha256').update(tmp).digest()
-}
+var cryptoHash = require('crypto-hashing')
 
 // Encode a buffer as a base58-check encoded string
 function encode (payload) {
-  var checksum = sha256x2(payload)
+  var checksum = cryptoHash('hash256', payload)
 
   return base58.encode(Buffer.concat([
     payload,
@@ -25,7 +18,7 @@ function decode (string) {
 
   var payload = buffer.slice(0, -4)
   var checksum = buffer.slice(-4)
-  var newChecksum = sha256x2(payload)
+  var newChecksum = cryptoHash('hash256', payload)
 
   if (checksum[0] ^ newChecksum[0] |
       checksum[1] ^ newChecksum[1] |
